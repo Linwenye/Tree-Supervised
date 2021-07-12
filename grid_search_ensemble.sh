e97893ac8aba072fd26b446f6693f17228eb4617
林文烨 <419766878@qq.com>
2021-07-12 17:00:41 +0800

    Delete grid_search_ensemble.sh

--- grid_search_ensemble.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for loss_efficient in 0.2 0.3 0.4
-do
-  for temperature in 2 3 4 5
-  do
-    CUDA_VISIBLE_DEVICES=0,1,2,3 python train_bi_ensemble.py --loss_coefficient $loss_efficient --temperature $temperature
-  done
-done
